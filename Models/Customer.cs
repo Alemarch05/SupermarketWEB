4efddaf7353099bb6ad8f957eb97c01f5de57502
alejandro <ale.marcho0514@gmail.com>
2025-05-10 14:44:38 -0500

    Index Customer

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -7,7 +7,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string? Address { get; set; }
-        public string? Birthday { get; set; }
+        public DateTime? Birthday { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Email { get; set; }
     }
