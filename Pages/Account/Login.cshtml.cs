2badd1a3400e138755c37dd282333a78be2b1f77
alejandro <ale.marcho0514@gmail.com>
2025-05-17 09:56:08 -0500

    Authorizate with DB

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
 using SupermarketWEB.Models;
 using System.Security.Claims;
 
@@ -8,6 +10,11 @@ namespace SupermarketWEB.Pages.Account
 {
     public class LoginModel : PageModel
     {
+        private readonly SupermarketContext _context;
+        public LoginModel(SupermarketContext _context)
+        {
+            this._context = _context;
+        }
         [BindProperty]
         public User User { get; set; }
 
@@ -16,14 +23,20 @@ namespace SupermarketWEB.Pages.Account
         {
            if (!ModelState.IsValid) return Page();
 
-            if (User.Email == "correo@gmail.com" || User.Password == "12345")
+            var userInDb = await _context.Users
+           .FirstOrDefaultAsync(u => u.Email == User.Email && u.Password == User.Password);
+            // me baso en el el index de user Users = await _context.Users.ToListAsync();
+            //El método FirstOrDefaultAsync devuelve un objeto del mismo tipo que el conjunto (DbSet<User>),
+            //es decir, un objeto de la clase User o null si no encuentra coincidencias.
+
+            if (userInDb != null)
             {
                 Console.Write("");
 
                 var claims = new List<Claim>
                 {
-                    new Claim(ClaimTypes.Role, "admin"),
-                    new Claim(ClaimTypes.Email,User.Email),
+                    new Claim(ClaimTypes.Role, userInDb.Role),
+                    new Claim(ClaimTypes.Email,userInDb.Email),
                 };
 
                 var indetity = new ClaimsIdentity(claims, "MyCookieAuth");
@@ -31,7 +44,7 @@ namespace SupermarketWEB.Pages.Account
                 ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(indetity);
 
                 await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
-                return RedirectToPage("/Index");
+                return RedirectToPage("/Index");    
             }
             return Page();
         }
